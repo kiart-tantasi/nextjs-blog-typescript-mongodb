5ca3cf2f8e033917358f28d64807d1bb850c1e8f
kiart-tantasi <petch_rwb@hotmail.com>
2022-02-25 16:53:55 +0700

    edited author section

--- components/ฺBlog/ArticleDetail.tsx
+++ components/ฺBlog/ArticleDetail.tsx
@@ -49,8 +49,9 @@ export default function ArticleDetail(props: Article) {
                 </section>
             </Card>
             <div className={styles.author}>
-                <p>ผู้เขียน - เพชร</p><br/>
-                <Link href="/aboutme">ประวัติผู้เขียน</Link>
+                <p>ผู้เขียน:</p><h3><Link href="/aboutme">เพชร</Link></h3>
+                <br/><br/>
+                <p>GitHub:</p><a href="https://github.com/kiart-tantasi" target="_blank" rel="noopener noreferrer"><h3>kiart-tantasi</h3></a>
             </div>
         </article>
         </>
