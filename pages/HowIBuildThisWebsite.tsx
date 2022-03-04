de908497a483559533e2e071db0a5ede069d722e
kiart-tantasi <petch_rwb@hotmail.com>
2022-03-04 18:53:09 +0700

    added Head for HowIBuild page

--- pages/HowIBuildThisWebsite.tsx
+++ pages/HowIBuildThisWebsite.tsx
@@ -1,8 +1,18 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import HowIBuildThisWebsitePage from "../components/ฺBlog/HowIBuildThisWebsitePage";
 
 const HowIBuildThisWebsite: NextPage = () => {
-    return <HowIBuildThisWebsitePage/>;
+    return (
+        <>
+        <Head>
+            <title>How I Build This Website</title>
+            <meta name="description" content="All Tech Stacks That I use in This Website" />
+
+        </Head>
+        <HowIBuildThisWebsitePage/>
+        </>
+    )
 }
 
 export default HowIBuildThisWebsite;
