f08eb06a200a0cef1d90ca864b466cdf9cafcb95
kiart-tantasi <petch_rwb@hotmail.com>
2022-03-12 20:44:02 +0700

    fixed CardUI in Preview Page

--- components/Form/CardPreview.tsx
+++ components/Form/CardPreview.tsx
@@ -2,20 +2,22 @@ import { ArticleTypes, PreviewData } from "../../interfaces/article";
 import CardUI from "../UI/CardUI";
 
 const CardPreview = (props:{previewData: PreviewData; category: ArticleTypes; slug: string}) => {
+    const articleCardData = {
+        index: 0,
+        _id: Math.random().toString(),
+        title: props.previewData.title,
+        desc: props.previewData.desc,
+        date: props.previewData.date,
+        img: props.previewData.img,
+        alt: props.previewData.alt,
+        slug: props.slug,
+        category: props.category
+    }
 
     return (
         <>
         <hr style={{marginBottom:"50px"}} />
-        <CardUI 
-        _id={Math.random().toString()}
-        title={props.previewData.title}
-        img={props.previewData.img}
-        alt={props.previewData.alt}
-        desc={props.previewData.desc}
-        date={props.previewData.date}
-        category={props.category}
-        slug={props.slug}
-        />
+        <CardUI articleCard={articleCardData} />
         <div style={{marginBottom:"100px"}} />
         </>
     )
