f8774b23b45b21f75f8fed21b07018fdc2cf1f1e
kiart-tantasi <petch_rwb@hotmail.com>
2022-03-14 20:15:13 +0700

    fixed transform img url (lib)

--- lib/transform-data.ts
+++ lib/transform-data.ts
@@ -1,61 +1,15 @@
 import { Db } from "mongodb";
-import getPresignedUrl from "./presigned-url";
+import AWS from "aws-sdk";
 
 import { Article, ArticleCard } from "../interfaces/article";
 
 export const transformCardData = async (array: Article[], db: Db): Promise<ArticleCard[]> => {
-    const justCreatedPresignedUrl: { [key: string]: string } = {};
     const result = [];
+    const secondArticleIdx = array.length - 2;
 
     for (let idx = 0; idx < array.length; idx++) {
         const x = array[idx];
-        let imgUrl = x.img;
-
-        // 1. IF PRIVATE BUCKET
-        if (x.img.includes("https://privatepetchdotblog.s3.ap-southeast-1.amazonaws.com")) {
-            const key: string = x.img.slice(60);
-            const presignedUrlCollection = db.collection("presignedUrl");
-            const result = await presignedUrlCollection.findOne({ key: key });
-            const justCreatedImgUrl = justCreatedPresignedUrl[key];
-
-            // 1.1 USE IMG URL THAT WAS JUST CREATED FROM PREVIOUS LOOPS (DUPLICATE IMG)
-            if (justCreatedImgUrl) {
-                imgUrl = justCreatedImgUrl;
-            }
-
-            // 1.2 PRESIGNED URL EXISTS AND DOES NOT EXPIRE
-            else if (result && result.age > new Date().getTime() - (5 * 60 * 1000)) {
-                imgUrl = result.url;
-            }
-
-            // 1.3 PRESIGNED URL DOES NOT EXIST OR ALREADY EXPIRED
-            else {
-                const newPresignedUrl = getPresignedUrl(key);
-                const dataToInsertOrUpdate = {
-                    key: key,
-                    url: newPresignedUrl,
-                    age: new Date().getTime() + 3_600_000
-                }
-                // 1.3.1 DOES NOT EXIST, INSERT
-                if (!result) await presignedUrlCollection.insertOne(dataToInsertOrUpdate);
-
-                //1.3.2 EXPIRED, UPDATE
-                else if (result.age) await presignedUrlCollection.updateOne({ key: key }, dataToInsertOrUpdate);
-
-                imgUrl = newPresignedUrl;
-                justCreatedPresignedUrl[key] = newPresignedUrl;
-            }
-
-            // LAST STEP: USE TRANSFER ACCELERATION IF IT IS IN LAST 2 INDEXES
-            if (idx >= array.length - 2 && imgUrl !== x.img) {
-                let params = imgUrl.slice(60);
-                imgUrl = "https://privatepetchdotblog.s3-accelerate.amazonaws.com/" + params;
-            }
-        }
-
-        // 2. IF PUBLIC BUCKET AND IT IS IN LAST 2 INDEXES
-        else if (idx >= array.length - 2 && x.img.includes("https://petchdotblog.s3.ap-southeast-1.amazonaws.com"))
-            imgUrl = "https://petchdotblog.s3-accelerate.amazonaws.com" + x.img.slice(52);
+        let imgUrl = await transformImgUrl(x.img, db, (idx >= secondArticleIdx));
 
         result.push({
             _id: x._id?.toString() || Math.random().toString(),
@@ -79,38 +33,51 @@ export const transformImgUrl = async(imgUrl: string, db: Db, accelerate: boolean
         const key: string = imgUrl.slice(60);
         const presignedUrlCollection = db.collection("presignedUrl");
         const result = await presignedUrlCollection.findOne({ key: key });
+        const deadline = (result)? result.age - (5 * 60 * 1000): null;
+        const now = new Date().getTime();
 
         // 1.1 PRESIGNED URL EXISTS AND DOES NOT EXPIRE
-        if (result && result.age > new Date().getTime() - (5 * 60 * 1000)) {
+        if (result && deadline && deadline > now) {
             imgUrl = result.url;
         }
-
+        
         // 1.2 PRESIGNED URL DOES NOT EXIST OR ALREADY EXPIRED
         else {
-            const newPresignedUrl = getPresignedUrl(key);
-            const dataToInsertOrUpdate = {
+            // CREATE NEW PRESIGNED URL (AWS SDK)
+            AWS.config.update({
+                accessKeyId: process.env.ACCESS_KEY_ID,
+                secretAccessKey: process.env.SECRET_ACCESS_KEY,
+                region: "ap-southeast-1"
+            });
+            const s3 = new AWS.S3();
+            let newPresignedUrl = s3.getSignedUrl('getObject', {
+                Bucket: process.env.BUCKET_NAME,
+                Key: key,
+                Expires: 60 * 60
+            });
+            const newPresignedUrlData = {
                 key: key,
                 url: newPresignedUrl,
                 age: new Date().getTime() + 3_600_000
             }
-            // 1.2.1 DOES NOT EXIST, INSERT
-            if (!result) await presignedUrlCollection.insertOne(dataToInsertOrUpdate);
 
-            //1.2.2 EXPIRED, UPDATE
-            else if (result.age) await presignedUrlCollection.updateOne({ key: key }, dataToInsertOrUpdate);
+            // 1.2.1 IF DOES NOT EXIST, INSERT
+            if (!result) await presignedUrlCollection.insertOne(newPresignedUrlData);
 
-            // set url
+            //1.2.2 IF EXPIRED, REPLACE
+            else if (deadline && deadline <= now) await presignedUrlCollection.replaceOne({ key: key }, newPresignedUrlData);
+
             imgUrl = newPresignedUrl;
         }
 
-        // LAST STEP: INDEX >= 1, transfer acceleration
+        // If Transfer Acceleration
         if (accelerate === true && imgUrl !== originalImgUrl) {
             let tempParams = imgUrl.slice(60);
             imgUrl = "https://privatepetchdotblog.s3-accelerate.amazonaws.com/" + tempParams;
         }
     }
     
-    // PUBLUC BUCKET
+    // 2. IF PUBLIC BUCKET (and If Transfer Acceleration)
     else if (accelerate === true && imgUrl.includes("https://petchdotblog.s3.ap-southeast-1.amazonaws.com")) {
         const tempParams = imgUrl.slice(52);
         imgUrl = "https://petchdotblog.s3-accelerate.amazonaws.com" + tempParams;
