5333e4ecfc04dbc508e7938190f149af3f7f442a
kiart-tantasi <petch_rwb@hotmail.com>
2022-03-29 03:50:39 +0700

    permanently deleted all component folders

--- components/blog/AboutMePage.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./AboutMePage.module.css";
-
-const AboutMePage = (props: {markdown:string;} ) => {
-    return <div className={`${styles.bio}`} dangerouslySetInnerHTML={{__html: props.markdown}} />;
-}
-
-export default AboutMePage;
