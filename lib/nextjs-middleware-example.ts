df0934f928d3176bc947ef8b8992f5a6d2eee09b
kiart-tantasi <petch_rwb@hotmail.com>
2022-06-04 00:39:02 +0700

    Add middleware example

---
+++ lib/nextjs-middleware-example.ts
@@ -0,0 +1,19 @@
+import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
+
+// MIDDLEWARE
+const isAuthenticated = (api: NextApiHandler) => (req: NextApiRequest, res: NextApiResponse) => {
+    
+    // check whether request is authenticated or not
+    const { user } = req.cookies;
+
+    // if not authenticated, response with 4xx
+    if (!user) res.status(403).json({message: "no permission"});
+
+    // if authenticated, just pass the request into API
+    else api(req, res);
+};
+
+// // API
+// export default isAuthenticated(async function api(req: NextApiRequest, res: NextApiResponse) {
+//     res.status(200).json({message: "user found !"});
+// });
