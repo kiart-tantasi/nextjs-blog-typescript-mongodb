fdd48ecb2b0ce90880c520eaf750f0451479d59a
kiart-tantasi <petch_rwb@hotmail.com>
2023-02-19 11:53:48 +0700

    force to have PRIVATE_KEY and refactor EnvGetter

--- lib/auth-edge.ts
+++ lib/auth-edge.ts
@@ -2,15 +2,23 @@ import { jwtVerify } from 'jose'
 
 // ref: https://github.com/vercel/examples/blob/main/edge-middleware/jwt-authentication/lib/auth.ts
 export async function verifyToken(token: string | undefined): Promise<boolean> {
-    try {
-        if (!token) {
-            throw new Error('token not found')
-        }
-        const verified = await jwtVerify(token, new TextEncoder().encode(getJwtKey()))
-        return !!verified.payload
-    } catch (err) {
+    if (!token) {
         return false
     }
+
+    // if env PRIVATE_KEY is not set, error will be thrown
+    const jwtKey = getJwtKey()
+
+    try {
+        const verified = await jwtVerify(token, new TextEncoder().encode(jwtKey))
+        const iat = verified.payload.iat
+        if (!!iat && (Date.now() > (iat * 1000))) {
+            return true
+        }
+    } catch {
+        // verifying failed
+    }
+    return false
 }
 
 // non-exported
@@ -18,5 +26,8 @@ export async function verifyToken(token: string | undefined): Promise<boolean> {
 const PRIVATE_KEY: string | undefined = process.env.PRIVATE_KEY
 
 function getJwtKey(): string {
-    return PRIVATE_KEY ?? ''
+    if (!PRIVATE_KEY || !PRIVATE_KEY.length) {
+        throw new Error('env PRIVATE_KEY is not set !')
+    }
+    return PRIVATE_KEY
 }
--- lib/env-getter.ts
+++ lib/env-getter.ts
@@ -1,32 +1,44 @@
 export class EnvGetter {
     static getDbUrl = (): string => {
-        return getEnv('DB_URL', process.env.DB_URL)
+        return getEnv('DB_URL')
     }
 
     static getPrivateKey = (): string => {
-        return getEnv('PRIVATE_KEY', process.env.PRIVATE_KEY)
+        return getEnv('PRIVATE_KEY')
     }
 
     static getAccessKeyId = (): string => {
-        return getEnv('ACCESS_KEY_ID', process.env.ACCESS_KEY_ID)
+        return getEnv('ACCESS_KEY_ID')
     }
 
     static getSecretAccessKey = (): string => {
-        return getEnv('SECRET_ACCESS_KEY', process.env.SECRET_ACCESS_KEY)
+        return getEnv('SECRET_ACCESS_KEY')
     }
 
     static getBucketName = (): string => {
-        return getEnv('BUCKET_NAME', process.env.BUCKET_NAME)
+        return getEnv('BUCKET_NAME')
     }
 
     static getPrivateBucketUrl = (): string => {
-        return getEnv('PRIVATE_BUCKET_URL', process.env.PRIVATE_BUCKET_URL)
+        return getEnv('PRIVATE_BUCKET_URL')
     }
 }
 
-function getEnv(envName: string, value: undefined | string): string {
-    if (value === undefined || value.length === 0) {
+// non-export
+
+function getEnv(envName: string): string {
+    const value = envs[envName]
+    if (!value || !value.length) {
         return `ENVIRONMENT VARIABLE "${envName}" IS NOT FOUND.`
     }
     return value
+}
+
+const envs: { [key: string]: string | undefined } = {
+    'DB_URL': process.env.DB_URL,
+    'PRIVATE_KEY': process.env.PRIVATE_KEY,
+    'ACCESS_KEY_ID': process.env.ACCESS_KEY_ID,
+    'SECRET_ACCESS_KEY': process.env.SECRET_ACCESS_KEY,
+    'BUCKET_NAME': process.env.BUCKET_NAME,
+    'PRIVATE_BUCKET_URL': process.env.PRIVATE_BUCKET_URL,
 }
