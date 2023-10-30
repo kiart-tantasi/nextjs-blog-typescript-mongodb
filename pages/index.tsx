6da15d3a3ba6512b36e82e99bec28d50162ea598
Kiart Tantasi <76615399+kiart-tantasi@users.noreply.github.com>
2023-10-30 20:12:14 +0700

    feat: Add canonical link (#16)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,53 @@
-import { MongoClient } from 'mongodb'
-import type { NextPage } from 'next'
+import { MongoClient } from "mongodb";
+import type { NextPage } from "next";
 
-import Articles from '../components/blog/Articles'
-import { Article, ArticleCard } from '../interfaces/article'
-import { EnvGetter } from '../lib/env-getter'
-import { transformCardData } from '../lib/transform-data'
+import Articles from "../components/blog/Articles";
+import { Article, ArticleCard } from "../interfaces/article";
+import { EnvGetter } from "../lib/env-getter";
+import { transformCardData } from "../lib/transform-data";
+import Head from "next/head";
 
 interface PageProps {
-    articles: ArticleCard[]
+  articles: ArticleCard[];
 }
 
 const Home: NextPage<{ articles: ArticleCard[] }> = (props: PageProps) => {
-    const articles = props.articles
-    return <Articles articles={articles} />
-}
+  const articles = props.articles;
+  return (
+    <>
+      <Head>
+        <link rel="canonical" href="https://www.petchblog.net/" />
+      </Head>
+      <Articles articles={articles} />
+    </>
+  );
+};
 
-export default Home
+export default Home;
 
-export async function getStaticProps(): Promise<{ props: { articles: ArticleCard[] } }> {
-    const dbUrl = EnvGetter.getDbUrl()
-    const client = new MongoClient(dbUrl)
+export async function getStaticProps(): Promise<{
+  props: { articles: ArticleCard[] };
+}> {
+  const dbUrl = EnvGetter.getDbUrl();
+  const client = new MongoClient(dbUrl);
 
-    let articles: ArticleCard[] = []
-    try {
-        await client.connect()
-        const db = client.db('blogDB')
-        const collection = db.collection('main')
-        const articlesFromDB = await collection.find({}).toArray()
-        articles = await transformCardData(articlesFromDB as unknown as Article[], db)
-        client.close()
-    } catch (error) {
-        client.close()
-    }
-    return {
-        props: {
-            articles,
-        },
-    }
+  let articles: ArticleCard[] = [];
+  try {
+    await client.connect();
+    const db = client.db("blogDB");
+    const collection = db.collection("main");
+    const articlesFromDB = await collection.find({}).toArray();
+    articles = await transformCardData(
+      articlesFromDB as unknown as Article[],
+      db
+    );
+    client.close();
+  } catch (error) {
+    client.close();
+  }
+  return {
+    props: {
+      articles,
+    },
+  };
 }
