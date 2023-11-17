583eaeb66e7596fdd7b5cb7a56a72dd51cfbb307
Kiart Tantasi <76615399+kiart-tantasi@users.noreply.github.com>
2023-11-17 16:53:50 +0700

    fix: wrong env var name

--- pages/sitemap.xml.ts
+++ pages/sitemap.xml.ts
@@ -5,7 +5,7 @@ import { MongoClient } from "mongodb";
 import { Article } from "../interfaces/article";
 
 function generateSiteMap(articles: Article[]) {
-  const domain: string = process.env.PUBLIC_DOMAIN || "http://localhost:3000";
+  const domain: string = process.env.NEXT_PUBLIC_DOMAIN || "http://localhost:3000";
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <!-- static urls -->
