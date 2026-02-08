bf776d20ba2c4a64308e8fe68b3fb38c05b7400a
kiart-tantasi <petch_rwb@hotmail.com>
2026-02-08 20:39:08 +0700

    add election page

---
+++ components/blog/ElectionPage.tsx
@@ -0,0 +1,11 @@
+
+const AboutMePage = (_props: { }) => {
+    return (
+        <div>
+            <h1>Election 2569</h1>
+            <p>In progress</p>
+        </div>
+    )
+}
+
+export default AboutMePage
---
+++ pages/election.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+import ElectionPage from '../components/blog/ElectionPage'
+
+const Election: NextPage = () => {
+    return <ElectionPage />
+}
+
+export default Election
